3d766d2969f7cc3785390f067d1b55699bc2b56a
Waseem Hassan <waseemhassanshahid@gmail.com>
2018-03-25 18:07:53 +0500

    Add support for additional things

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search'
@@ -16,7 +17,11 @@ class App extends Component {
             selectedVideo: null
         };
 
-        YTSearch({key: API_KEY, term: 'danish ali'}, (videos) => {
+        this.videoSearch('danish ali');
+    }
+
+    videoSearch(term) {
+        YTSearch({key: API_KEY, term: term}, (videos) => {
             this.setState({
                 videos: videos,
                 selectedVideo: videos[0]
@@ -24,12 +29,12 @@ class App extends Component {
         });
     }
 
-    
-
     render() {
+        const videoSearchFunc = _.debounce((term) => { this.videoSearch(term) }, 300);
+
         return (
             <div>
-                <SearchBar />
+                <SearchBar onSearchTermChange={videoSearchFunc} />
                 <VideoDetail video={this.state.selectedVideo} />
                 <VideoList
                     onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
